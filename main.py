8986cd54863900251d891585f77daba2110c1b80
SeetZy <64326465+SeetZy@users.noreply.github.com>
2022-03-11 00:06:00 +0200

    Basic browser needs improvement

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtWebEngineWidgets import *
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class OrchidWeb(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(OrchidWeb, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        search_bar = QToolBar()
+        self.addToolBar(search_bar)
+
+        btn_back = QAction('←', self)
+        btn_back.triggered.connect(self.browser.back)
+        search_bar.addAction(btn_back)
+
+        btn_forward = QAction('→', self)
+        btn_forward.triggered.connect(self.browser.forward)
+        search_bar.addAction(btn_forward)
+
+        btn_refresh = QAction('↺', self)
+        btn_refresh.triggered.connect(self.browser.reload)
+        search_bar.addAction(btn_refresh)
+
+        btn_home = QAction('⌂', self)
+        btn_home.triggered.connect(self.home)
+        search_bar.addAction(btn_home)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.to_url)
+        search_bar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def home(self):
+        self.browser.setUrl(QUrl('http://google.com'))
+
+    def to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName("Orchid Web")
+window = OrchidWeb()
+app.exec_()
+
